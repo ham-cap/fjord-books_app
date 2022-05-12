34eaf393dfb2fb056382864ac250bad831e631d5
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-05-12 22:09:29 +0900

    ユーザーの一覧表示・ログイン済みか否かの判定・ログアウト機能をそれぞれ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :books
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  resources :users, :only => [:index, :show]
+
+  devise_scope :user do
+    post '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  ## 開発環境用letter_opener
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
