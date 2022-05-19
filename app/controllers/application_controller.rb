ff670e4c256716dad1a96611419e8e4e7f98ed41
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-05-20 06:21:08 +0900

    ログインとログアウト時のリダイレクト先を変更・ユーザー一覧に表示している情報を詳細画面へのリンクに変更・ユーザー詳細画面の追加
    ・ログインとログアウトの際に/までリダイレクトされ、別のページに移動するにはURLを直接打ち込むしかなかったため、
    それぞれ本の一覧とログイン画面に遷移するよう変更。
    ・ユーザー一覧に表示している情報（メールアドレス）をユーザー詳細画面へのリンクに変更し、ユーザー詳細画面のviewも一部修正。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource_or_scope)
+    books_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
