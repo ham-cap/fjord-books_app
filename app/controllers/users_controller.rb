52041dfe5f46de504dd1136ec5462fe87e6e6120
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-07-03 14:19:14 +0900

    ユーザー一覧をページング処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.order(:id).page(params[:page])
   end
 
   def show
