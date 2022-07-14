e2c628980dc18638fe44e1ba3ce0df66a7e4659f
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-07-14 20:00:11 +0900

    マイグレーションファイルを修正
    ・zipcodeカラムのデータ型をintegerからtextに変更する際、新規でマイグレーションファイルを作成し、upとdownで修正していたが
    マージ前のもののため、カラムを追加した際のマイグレーションファイルを修正することとした。
    データ型変更のためだけに作ったマイグレーションファイルは削除した。

--- db/migrate/20220523212231_add_zipcode_address_introduce_to_users.rb
+++ db/migrate/20220523212231_add_zipcode_address_introduce_to_users.rb
@@ -1,6 +1,6 @@
 class AddZipcodeAddressIntroduceToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :zipcode, :integer
+    add_column :users, :zipcode, :text
     add_column :users, :address, :text
     add_column :users, :introduce, :text
   end
--- db/migrate/20220529030658_change_datatype_zipcode_of_users.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDatatypeZipcodeOfUsers < ActiveRecord::Migration[6.1]
-  def up
-    change_column :users, :zipcode, :text
-  end
-
-  def down
-    change_column :users, :zipcode, :integer
-  end
-end
