de91c49a853f3f090d73a1855035f5e203a64808
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-10-30 10:03:40 +0900

    不要なアクションとjsonレスポンス部分を削除・noticeをi18n対応
    ・comments_controllerではcreateとshow以外のアクションを使用しないため削除した
    ・今回はjsonのレスポンスはしないため該当部分は削除した

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,54 +3,16 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[show edit update destroy]
 
-  # GET /comments or /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1 or /comments/1.json
+  # GET /comments/1
   def show; end
 
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit; end
-
-  # POST /comments or /comments.json
+  # POST /comments
   def create
     @comment = @commentable.comments.new(user_id: current_user.id, content: comment_params[:content])
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to [@commentable, @comment], notice: 'Comment was successfully created.' }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to comment_url(@comment), notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /comments/1 or /comments/1.json
-  def destroy
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
+    if @comment.save
+      redirect_to [@commentable, @comment], notice: t('controllers.common.notice_create', name:Comment.model_name.human)
+    else
+      render file: "#{Rails.root}/public/422.html", layout: false
     end
   end
 
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,12 +4,12 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
-  # GET /reports or /reports.json
+  # GET /reports
   def index
     @reports = Report.all.order(created_at: :DESC)
   end
 
-  # GET /reports/1 or /reports/1.json
+  # GET /reports/1
   def show
     @report = Report.find(params[:id])
     @comments = @report.comments.includes(:user)
@@ -26,38 +26,30 @@ class ReportsController < ApplicationController
     @user_id = current_user.id
   end
 
-  # POST /reports or /reports.json
+  # POST /reports
   def create
     @report = Report.new(report_params)
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: 'Report was successfully created.' }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name:Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
+  # PATCH/PUT /reports/1
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: 'Report was successfully updated.' }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name:Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /reports/1 or /reports/1.json
+  # DELETE /reports/1
   def destroy
     @report.destroy
-
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: 'Report was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name:Report.model_name.human)
   end
 
   private
