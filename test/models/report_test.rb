dc5e7c2390af57ee5d68b94f3627663d45487a2e
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-11-23 08:15:02 +0900

    report_test.rbで使用するユーザーのデータをfixturesのものに変更・created_onメソッドのテストで出力の比較対象となるオブジェクトのクラスを変更
    ・report_test.rbで使用するユーザーのデータはnewを使ってその場で生成していたが、DBへ保存されずidも付番されないことから、fixturesのデータを使用することとした
    ・created_onの出力結果をDateTimeクラスのインスタンスにto_dateを使用した結果と比較していたが、よりわかりやすさを重視するため最初からDateクラスのインスタンスとして生成することとした

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,17 +4,17 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test 'editable?' do
-    user1 = User.new(email: 'foo@example.com')
-    user2 = User.new(email: 'bar@example.com')
+    user1 = users(:alice)
+    user2 = users(:bob)
     report = Report.new(user_id: user1.id, title: 'テスト', content: 'foobar')
-    assert report.editable?(user1.id)
+    assert report.editable?(user1)
     assert_not report.editable?(user2)
   end
 
   test 'created_on' do
     user1 = User.new(email: 'foo@example.com')
     report = Report.new(user_id: user1.id, title: 'テスト', content: 'foobar', created_at: DateTime.new(1993, 2, 24, 12, 30, 45))
-    date = DateTime.new(1993, 2, 24, 12, 30, 45).to_date
+    date = Date.new(1993, 2 ,24)
     assert_equal date, report.created_on
   end
 end
