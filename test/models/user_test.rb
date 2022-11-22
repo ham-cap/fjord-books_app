b3904b37a8a99fa9c11938f8c2c5b770b235e43e
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-11-23 08:51:53 +0900

    user_test.rb内で使用するユーザーのデータをfixturesのものに変更・各メソッドの実行前後の変化が分かるように修正・followは他のメソッドのテスト内で動作確認ができるため削除

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,31 +4,27 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test '#following?' do
-    user1 = User.new(email: 'foo@example.com')
-    user2 = User.new(email: 'bar@example.com')
-    assert_equal false, user1.following?(user2)
+    alice = users(:alice)
+    bob = users(:bob)
+    assert_not alice.following?(bob)
   end
 
   test '#followed_by?' do
-    user1 = User.create!(email: 'foo@example.com', password: 'password')
-    user2 = User.create!(email: 'bar@example.com', password: 'password')
-    user1.follow(user2)
-    assert user2.followed_by?(user1)
-  end
-
-  test '#follow' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-    me.follow(she)
-    assert me.following?(she)
+    alice = users(:alice)
+    bob = users(:bob)
+    assert_not bob.followed_by?(alice)
+    alice.follow(bob)
+    assert bob.followed_by?(alice)
   end
 
   test '#unfollow' do
-    me = User.create!(email: 'me_unfollow@example.com', password: 'password')
-    she = User.create!(email: 'she_unfollow@example.com', password: 'password')
-    me.follow(she)
-    me.unfollow(she)
-    assert_not me.following?(she)
+    alice = users(:alice)
+    bob = users(:bob)
+    assert_not alice.following?(bob)
+    alice.follow(bob)
+    assert alice.following?(bob)
+    alice.unfollow(bob)
+    assert_not alice.following?(bob)
   end
 
   test '#name_or_email' do
