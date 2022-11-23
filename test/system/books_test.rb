d798d2abb9c4e03343ed26f5d9f0c08bc7309e81
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-11-23 12:25:29 +0900

    Booksのシステムテストの内容を修正
    ・新規作成や更新の際に使用するデータはfixturesを使用していたが、テストの開始時点でそれらのデータが登録された状態となってしまうため適切にテストできていなかった。そのため新規作成はfixturesを参照せずに文字列をそのまま使用し、更新と削除はfixturesに1件だけ用意したデータに対して実施することとした。

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,8 +4,6 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
-
     visit root_url
     fill_in 'Eメール', with: 'alice@example.com'
     fill_in 'パスワード', with: 'password'
@@ -17,32 +15,48 @@ class BooksTest < ApplicationSystemTestCase
     assert_selector 'h1', text: '本'
   end
 
-  test 'creating a Book' do
+  test 'creating and destroying a Book' do
     visit books_url
     click_on '新規作成'
-    fill_in 'タイトル', with: @book.title
-    fill_in 'メモ', with: @book.memo
-    fill_in '著者', with: @book.author
+    fill_in 'タイトル', with: 'hogehoge'
+    fill_in 'メモ', with: 'Interesting!'
+    fill_in '著者', with: 'Me'
     click_on '登録する'
     assert_text '本が作成されました。'
+    click_on '戻る'
+    assert_text 'hogehoge'
+    assert_text 'Interesting!'
+    assert_text 'Me'
   end
 
   test 'updating a Book' do
     visit books_url
+    assert_text 'fugafuga'
+    assert_text 'Excellent!'
+    assert_text 'Bar'
     click_on '編集', match: :prefer_exact
-    fill_in 'タイトル', with: @book.title
-    fill_in 'メモ', with: @book.memo
-    fill_in '著者', with: @book.author
-    click_on '更新する'
+    fill_in 'タイトル', with: 'piyopiyo'
+    fill_in 'メモ', with: 'Great!'
+    fill_in '著者', with: 'Foo'
+    click_button '更新する'
     assert_text '本が更新されました。'
+    click_on '戻る'
+    assert_text 'piyopiyo'
+    assert_text 'Great!'
+    assert_text 'Foo'
   end
 
   test 'destroying a Book' do
     visit books_url
+    assert_text 'fugafuga'
+    assert_text 'Excellent!'
+    assert_text 'Bar'
     page.accept_confirm do
       click_on '削除', match: :first
     end
-
     assert_text '本が削除されました。'
+    assert_no_text 'fugafuga'
+    assert_no_text 'Excellent!'
+    assert_no_text 'Bar'
   end
 end
